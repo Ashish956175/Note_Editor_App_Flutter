d15dd33ecad0a93212d46c25708a47f8d2641a4e
Ashish956175 <ashishgaikwad9561@gmail.com>
2026-09-20 14:33:51 +0530

    feat: connect note UI with SQLite CRUD operations

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -12,8 +12,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  DbHelper? dbHelper;
+  List<Map<String, dynamic>> notes=[];
+  var titleController = TextEditingController();
+  var descController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DbHelper.getInstance();
+    getAllNotes();
+  }
+  Future<void>getAllNotes() async {
+    notes = await dbHelper!.getAllNotes();
+    setState((){});
+  }
 
-  DbHelper dbHelper = DbHelper.getInstance();
   @override
   Widget build(BuildContext context) {
 
@@ -29,19 +42,118 @@ class _MyHomePageState extends State<MyHomePage> {
           height: double.infinity,
           color: Colors.amber.shade200,
           child: ListView.builder(
-            itemCount: 25,
+            itemCount: notes.length,
               itemBuilder: (_,index){
             return Card(
               margin: EdgeInsets.all(7),
+              color: index.isEven ? Colors.amber.shade50 : Colors.amber
+                  .shade100,
               child: ListTile(
-                title: Text("title"),
-                subtitle: Text("description"),
+                leading: Icon(Icons.note_alt_outlined,size: 31,),
+                title: Text(notes[index][DbHelper.columnTitle]),
+                subtitle: Text(notes[index][DbHelper.columnDesc]),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(onPressed: (){
+                      titleController.text = notes[index][DbHelper.columnTitle];
+                      descController.text = notes[index][DbHelper.columnDesc];
+                      showModalBottomSheet(context: context, builder: (_){
+
+                        return Container(
+                          color: Colors.amber.shade50,
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Text("Update Notes",style: TextStyle(fontSize:
+                                21),),
+                                SizedBox(height: 20,),
+                                TextField(
+                                  controller: titleController,
+                                  decoration: InputDecoration(label: Text("title",style:
+                                  TextStyle(fontSize: 21))
+                                      ,hint: Text("Enter Title", style: TextStyle(fontSize:
+                                      17,),),
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      border:
+                                      OutlineInputBorder(borderRadius: BorderRadius.circular(21),),
+                                      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(21),
+                                          borderSide: BorderSide(color: Colors.amber.shade800)),
+                                      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(21),
+                                        borderSide: BorderSide(width: 2, color: Colors.amber
+                                            .shade800),)
+
+                                  ),
+
+                                ),
+                                SizedBox(height: 20,),
+                                TextField(
+                                  controller: descController,
+                                  maxLines: 4,
+                                  decoration: InputDecoration(label: Text("description",style:
+                                  TextStyle(fontSize: 21),)
+                                    ,hint: Text("Enter Description", style: TextStyle
+                                      (fontSize:
+                                    17),),
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    border:
+                                    OutlineInputBorder(borderRadius: BorderRadius.circular(21),),
+                                    enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(21),
+                                        borderSide: BorderSide(color: Colors.amber.shade800)),
+                                    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(21),
+                                      borderSide: BorderSide(width: 2, color: Colors.amber
+                                          .shade800),),
+                                  ),
+
+
+                                ),
+                                SizedBox(height: 20,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(onPressed: () async {
+                                      await dbHelper!.updateNote(id: notes[index][DbHelper.columnId],
+                                          title: titleController.text, desc: descController.text)? print("success"):print("failed");
+                                      titleController.clear();
+                                      descController.clear();
+                                      Navigator.pop(context);
+                                      getAllNotes();
+                                    }, child: Icon(Icons.save,
+                                      color: Colors.lightGreen.shade900,size: 31,)),
+                                    SizedBox(width: 20),
+                                    ElevatedButton(onPressed: (){
+                                      Navigator.pop(context);
+                                    }, child: Icon(Icons.close,color: Colors.red.shade900,
+                                      size: 31,))
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                    }, child: Icon(Icons.edit,
+                      color: Colors.amber.shade900,),),
+                    SizedBox(width: 10,),
+                    ElevatedButton(onPressed: () async {
+                      await dbHelper!.deleteNote(id: notes[index][DbHelper.columnId]);
+                      getAllNotes();
+                    }, child: Icon(Icons.delete,color: Colors.red.shade900,))
+
+                  ]
+                ),
               ),
             );
           })
         ),
       floatingActionButton: FloatingActionButton(onPressed: (){
         showModalBottomSheet(context: context, builder: (_){
+          titleController.clear();
+          descController.clear();
           return Container(
             color: Colors.amber.shade50,
             width: double.infinity,
@@ -52,6 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   Text("Add Notes",style: TextStyle(fontSize: 21),),
                   SizedBox(height: 20,),
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(label: Text("title",style:
                     TextStyle(fontSize: 21))
                         ,hint: Text("Enter Title", style: TextStyle(fontSize:
@@ -71,6 +184,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   SizedBox(height: 20,),
                   TextField(
+                    controller: descController,
                     maxLines: 4,
                     decoration: InputDecoration(label: Text("description",style:
                     TextStyle(fontSize: 21),)
@@ -94,7 +208,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      ElevatedButton(onPressed: (){}, child: Icon(Icons.save,
+                      ElevatedButton(onPressed: () async {
+                        await dbHelper!.addNote(title: titleController.text, desc:
+                        descController.text)? print("success"):print("failed");
+                        titleController.clear();
+                        descController.clear();
+                        Navigator.pop(context);
+                        getAllNotes();
+                      }, child: Icon(Icons.save,
                         color: Colors.lightGreen.shade900,size: 31,)),
                       SizedBox(width: 20),
                       ElevatedButton(onPressed: (){
--- lib/db_Helper.dart
+++ lib/db_Helper.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:sqflite/sqflite.dart';
@@ -13,33 +12,26 @@ class DbHelper {
   static const String columnDesc = "description";
   static const String columnCurrentTime = "createdAt";
 
-
   DbHelper._();
   static DbHelper getInstance() => DbHelper._();
-
   Database? mdb;
-
   //intidb
   Future<Database> initDb() async {
     mdb ??= await openDb();
     return mdb!;
 
-
   }
-
   //opendb
   Future<Database> openDb() async{
-    Directory appDir = await pathProvider.getApplicationDocumentsDirectory();
-    String path =  join(dbname, appDir.path);
+    var appDir = await pathProvider.getApplicationDocumentsDirectory();
+    String path =  join(appDir.path, dbname);
     return openDatabase(path, version: 1, onCreate: (db,_){
       db.execute("create table notes($columnId integer primary key autoincrement, $columnTitle text, $columnDesc text, $columnCurrentTime text)");
     });
   }
-
-
   //crude
   //add
- Future<bool> addNote({required String title, required String desc})async {
+  Future<bool> addNote({required String title, required String desc})async {
    Database db = await initDb();
    return await db.insert(tablename, {
      columnTitle: title,
@@ -50,7 +42,6 @@ class DbHelper {
          .toString()}) > 0;
  }
    //update
-
   Future<bool> updateNote({required int id, required String title, required String
   desc})async{
     Database db = await initDb();
@@ -59,16 +50,13 @@ class DbHelper {
       columnDesc: desc,
     },where: '$columnId = ?' , whereArgs: [id])>0;
   }
-
   //fetching notes data
-
-  Future<List<Map<String, dynamic>>> notes() async{
+  Future<List<Map<String, dynamic>>> getAllNotes() async{
     Database db = await initDb();
     return await db.query(tablename);
   }
-
   //delete
- Future<bool> delete({required int id})async {
+ Future<bool> deleteNote({required int id})async {
    Database db = await initDb();
    return await db.delete(tablename, where: '$columnId = ?', whereArgs: [id]) > 0;
  }
